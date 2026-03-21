5432e57d1f188762d5fd6b3701799149a83a50ef
Linda Liu <lindaliu444@gmail.com>
2026-03-20 22:13:30 -0700

    update main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,13 @@ export default function Home() {
       <ul>
         <li>computer science student at the university of british columbia (graduating may 2026)</li>
         <li>previous software developer intern @ sap</li>
-        <li>interested in data analysis/science, relational databases, and software development</li>
-        <li>learning pottery at vancouver ceramics studio</li>
+        <li>interested in relational databases, software development, and the data lifecycle</li>
+        <li>learning wheel pottery and minecraft speedrunning in my free time</li>
       </ul>
-      <p style={{ marginBottom: "5rem" }}>i'd love to talk! feel free to reach out through <a className="social-media-links" href="mailto:lindaliu444@gmail.com">email</a> or <a className="social-media-links" href="https://www.linkedin.com/in/linda-liu1/">linkedin</a>.
-      or, you can find me on <a className="social-media-links" href="https://github.com/lindaliu1">github</a>, <a className="social-media-links" href="https://open.spotify.com/user/lindamaybe?si=95f68b1e225d4316">spotify</a>, and <a className="social-media-links" href="https://beliapp.co/app/lindaliu">beli</a>!</p>
+      <p style={{ marginBottom: "5rem" }}>i'd love to talk! feel free to reach out through <a className="social-media-links" href="https://www.linkedin.com/in/linda-liu1/">linkedin</a>.
+      you can also find me on <a className="social-media-links" href="https://github.com/lindaliu1">github</a>, <a className="social-media-links" href="https://open.spotify.com/user/lindamaybe?si=95f68b1e225d4316">spotify</a>, and <a className="social-media-links" href="https://beliapp.co/app/lindaliu">beli</a>!</p>
 
-      <p>last updated: december 2025</p>
+      <p>last updated: march 2026</p>
     </>
   );
 }
